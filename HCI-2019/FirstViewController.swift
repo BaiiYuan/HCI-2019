f128443773fbb8a3e2722749a24933cb6cdac707
Byron.lee <byron.lee@grindr.com>
2019-05-12 15:36:55 +0800

    init

---
+++ HCI-2019/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  HCI-2019
+//
+//  Created by 李栢淵 on 2019/5/12.
+//  Copyright © 2019 李栢淵. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
