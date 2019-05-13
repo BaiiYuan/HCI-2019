21f62f3820aeb8557a0bbf1f6145df5f6cbf5337
Po-wen Chen <jacky87810@gmail.com>
2019-05-13 23:48:35 +0800

    Revise structure

--- HCI-2019/homepageViewController.swift
+++ HCI-2019/homepageViewController.swift
@@ -15,7 +15,11 @@ class homepageViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
